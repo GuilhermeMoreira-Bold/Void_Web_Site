69ad2420d1d86e21c2b2331c09dc52f13939717e
Guigo <gulhermejacinto1@gmail.com>
2023-10-08 17:14:41 -0300

    Testing

---
+++ src/main/java/com/example/vvweb/VvWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.vvweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vvweb/VvWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vvweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
